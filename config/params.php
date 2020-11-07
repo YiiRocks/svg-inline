24c2c37f370a2affb4fe9b18dab1e7b630e034c6
Mr.42 <johan@mr42.me>
2020-11-07 11:25:38 +0100

    Automatically inject into view

--- config/params.php
+++ config/params.php
@@ -2,9 +2,18 @@
 
 declare(strict_types=1);
 
+use YiiRocks\SvgInline\SvgInlineInterface;
+use Yiisoft\Factory\Definitions\Reference;
+
 return [
     'yiirocks/svg-inline' => [
         'fallbackIcon' => '@vendor/yiirocks/svg-inline/src/fallbackIcon.svg',
         'fill' => 'currentColor',
+    ],
+
+    'yiisoft/view' => [
+        'defaultParameters' => [
+            'svg' => Reference::to(SvgInlineInterface::class),
+        ],
     ],
 ];
