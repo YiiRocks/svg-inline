2bcc4d164f839ca2eb8b4780d2e6d584b791fcdd
Mister-42 <johan@mr42.me>
2025-03-08 23:01:25 +0100

    Remove view-renderer config

--- config/params.php
+++ config/params.php
@@ -10,12 +10,4 @@ return [
         'fallbackIcon' => '@vendor/yiirocks/svg-inline/src/fallbackIcon.svg',
         'fill' => 'currentColor',
     ],
-
-    'yiisoft/yii-view-renderer' => [
-        'viewPath' => null,
-        'layout' => null,
-        'injections' => [
-            Reference::to(SvgViewInjection::class),
-        ],
-    ],
 ];
