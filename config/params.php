aefcf16432a7e055d508c7b10568d011ef2f9959
Mister-42 <johan@mr42.me>
2025-03-10 15:45:30 +0100

    Injections config

--- config/params.php
+++ config/params.php
@@ -2,12 +2,25 @@
 
 declare(strict_types=1);
 
-use YiiRocks\SvgInline\SvgViewInjection;
+use YiiRocks\SvgInline\SvgInjections;
+use YiiRocks\SvgInline\SvgInlineInterface;
 use Yiisoft\Definitions\Reference;
 
 return [
     'yiirocks/svg-inline' => [
         'fallbackIcon' => '@vendor/yiirocks/svg-inline/src/fallbackIcon.svg',
         'fill' => 'currentColor',
+    ],
+
+    'yiisoft/view' => [
+        'parameters' => [
+            'svg' => Reference::to(SvgInlineInterface::class),
+        ],
+    ],
+
+    'yiisoft/yii-view-renderer' => [
+        'injections' => [
+            Reference::to(SvgInjections::class),
+        ],
     ],
 ];
